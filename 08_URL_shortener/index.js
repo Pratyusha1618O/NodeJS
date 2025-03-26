fa279cd25a6a62209525b4107bc7980d8e527c5f
Pratyusha1618O <pratyushabhandary2003@gmail.com>
2025-03-26 23:57:47 +0530

    URL Shortener

--- 08_URL_shortener/index.js
+++ 08_URL_shortener/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const URLroute = require("./routes/url");
 const { connectMongoDB } = require("./connections.js");
 const URL = require("./models/url.js");
+const path = require('path')
 
 const app = express();
 const PORT = 8000;
@@ -10,9 +11,20 @@ connectMongoDB("mongodb://localhost:27017/shortURL");
 
 app.use(express.json());
 
+app.set("view engine", 'ejs');
+app.set("views", path.resolve("./views"))
+
+// server side rendering
+app.get("/test", async (req, res)=>{
+  const allUrls = await URL.find({});
+  return res.render("home", {
+    urls: allUrls,
+  })
+})
+
 app.use("/url", URLroute);
 
-app.get("/:shortId", async (req, res) => {
+app.get("/url/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
   const entry = await URL.findOneAndUpdate(
     {
